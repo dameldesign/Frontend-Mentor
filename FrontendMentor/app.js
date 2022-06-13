b72045723756e8c35ccfbc6f4623fc0e5ffcf7fb
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-06-13 21:26:44 +0100

    to be submitted

---
+++ FrontendMentor/app.js
@@ -0,0 +1,22 @@
+const menuButton = document.querySelector("#open");
+const closeButton = document.querySelector("#close");
+const navContainer = document.querySelector(".nav__container");
+
+menuButton.onclick = (e) => {
+  navContainer.classList.add("show");
+  closeButton.classList.add("reveal");
+  menuButton.classList.add("hide");
+};
+
+closeButton.onclick = (e) => {
+  navContainer.classList.remove("show");
+  closeButton.classList.remove("reveal");
+  menuButton.classList.remove("hide");
+};
+
+navContainer.addEventListener("mousemove", (e) => {
+  e.clientX <= 0.315 * document.documentElement.clientWidth &&
+  navContainer.classList.remove("show");
+  closeButton.classList.remove("reveal");
+  menuButton.classList.remove("hide");
+});
